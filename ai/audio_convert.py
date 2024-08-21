a485049ee86d178d7461bd418e16680a37019b1a
Woo <white_spade@naver.com>
2024-08-21 15:53:26 +0900

    Feat:Update bitrate error handle
    AI 인식 성능을 높이기 위해 압력음성파일이 wav/16000 bitrate를 유지하도록 코드를 변환함

--- ai/audio_convert.py
+++ ai/audio_convert.py
@@ -1,8 +1,11 @@
 from pydub import AudioSegment
+from pydub.utils import mediainfo
 
 def wav_converter(speechfile):
-    if speechfile.split('.')[1] != "wav":
+    info = mediainfo(speechfile)
+    if(info['sample_rate'] != 16000 or speechfile.split('.')[1] != "wav"):
         sound = AudioSegment.from_file(speechfile)
+        sound = sound.set_frame_rate(16000)
         speechfile = speechfile.split('.')[0]+".wav"
         sound.export(speechfile, format="wav", bitrate="128k")
-        return speechfile
+    return speechfile
